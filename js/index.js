da2ef9cd334cb50bd7943bb59b686f5b2e03c69b
unknown <cejasjoaquin1998@gmail.com>
2022-06-16 01:34:01 -0300

    primer sitio web

---
+++ js/index.js
@@ -0,0 +1,54 @@
+window.sr = ScrollReveal();
+  sr.reveal('.firstRoll', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.navRoll', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.secondRoll', {
+    duration: 5000,
+    origin: 'right',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.thirdRoll', {
+    duration: 5000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.fourRoll', {
+    duration: 6000,
+    origin: 'left',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.htmlRoll', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.cssRoll', {
+    duration: 4000,
+    origin: 'top',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.jsRoll', {
+    duration: 5000,
+    origin: 'top',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.reactRoll', {
+    duration: 6000,
+    origin: 'top',
+    distance: '-100px'
+  });
